f45e0a281ddcd25f0ae87a1fa2b88e0848225c2c
stephancarpin <viviancarpin@gmail.com>
2022-08-18 11:48:10 +0400

    add image save using agg

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
+from machine_learning import *
+from time import sleep
 
+
+
+
+from threading import Thread
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b1808f24613321f9007f0e8b31759bc269e8fc6a6a2fb51d'
+
+
+
+
+
+
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -12,11 +25,20 @@ messages = [{'title': 'Message One',
 def index():
     return render_template('index.html', messages=messages)
 
+
+
+
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
-        title = request.form['title']
+        title   = request.form['title']
         content = request.form['content']
+
+
+
+
+        perform_task()
+
 
         if not title:
             flash('Title is required!')
---
+++ machine_learning.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from pandas import read_csv
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.switch_backend('Agg')
+print("Using:",matplotlib.get_backend())
+
+
+
+
+def perform_task():
+    data = pd.read_csv("./data/dataset.csv")
+    data.shape
+    df = pd.DataFrame(data)
+    print(df)
+    x = df.iloc[:,:-2]
+    print(x)
+    y = df.iloc[: , -2:]
+    print(y)
+    ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [4])], remainder= 'passthrough')
+    x = np.array(ct.fit_transform(x))
+
+    print(x)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2,random_state= 1)
+    sc_x=StandardScaler()
+    x_train=sc_x.fit_transform(x_train)
+    x_test=sc_x.transform(x_test)
+    x_train
+
+    # Creating data
+    year = ['2010', '2002', '2004', '2006', '2008']
+    production = [25, 15, 35, 30, 10]
+
+    # Plotting barchart
+    plt.bar(year, production)
+
+    # Saving the figure.
+    plt.savefig("./data/output.jpg")
+
+    # Saving figure by changing parameter values
+    plt.savefig("./data/output1", facecolor='y', bbox_inches="tight",
+                                pad_inches=0.3, transparent=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
